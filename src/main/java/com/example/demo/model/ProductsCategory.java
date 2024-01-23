42d45c728829ea4c6d40de069a94460348131f8c
mikhailbalagurov <balagurovmikhail@gmail.com>
2024-01-23 16:51:03 +0300

    added categories of products

---
+++ src/main/java/com/example/demo/model/ProductsCategory.java
@@ -0,0 +1,5 @@
+package com.example.demo.model;
+
+public enum ProductCategory {
+    Bed, Table, Wardrobe, Chair, ArmChair, Other;
+}
--- src/main/resources/db/migration/V1__init_database.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists products
-(
-    id serial not null
-        primary key,
-    name varchar not null,
-    price int not null,
-    description varchar not null
-);
